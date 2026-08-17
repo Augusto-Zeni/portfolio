3b94256a01003ab754ed0dc87d0478ff7a96b21f
Augusto Zeni <augustozeni955@gmail.com>
2026-08-16 22:15:46 -0300

    feat: add font, header and hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Roboto Mono',
+      cssVariable: '--font-roboto-mono',
+      weights: [500, 700],
+      styles: ['normal'],
+    },
+  ],
+
+  integrations: [react()],
 })
